4023800574f8fef5a940aa5e36e7bf74789743c7
Vishal S Bhat <54361223+vishalSBhat@users.noreply.github.com>
2021-05-08 19:58:09 +0530

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    const q = $('.disable-sub-question');
+    if(q.length == 0)
+    return;
+
+    const id = q.attr('data-id');
+    $(`div[data-main-question='${id}']`).addClass('disable-input');
+});
+
+$('input').click(function(e){
+    const q = $('.disable-sub-question');
+    if(q.length == 0)
+    return;
+
+
+    const id = q.attr('data-id'), a = $(e.target), value = $(`label[for=${a.attr('id')}]`).text();
+
+    if(id == a.attr('name'))
+    if(value == 'Yes')
+    $(`div[data-main-question='${id}']`).removeClass('disable-input');
+    else{
+    $(`div[data-main-question='${id}']`).addClass('disable-input');
+    $(`div[data-main-question='${id}'] input`).val('')}
+    
+});
