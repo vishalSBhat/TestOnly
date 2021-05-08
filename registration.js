7d887afc9525d33121081833be5e63aeaf3b1a83
Vishal S Bhat <54361223+vishalSBhat@users.noreply.github.com>
2021-05-08 21:01:02 +0530

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,15 +1,4 @@
-$(document).ready(function() {
-    console.log('hello');
-    const q = $('.disable-sub-question');
-    if(q.length == 0)
-    return;
-
-    const id = q.attr('data-id');
-    $("div[data-main-question='"+id+"']").addClass('disable-input');
-});
-
 $('input').click(function(e){
-    console.log('input');
     const q = $('.disable-sub-question');
     if(q.length == 0)
     return;
@@ -18,10 +7,13 @@ $('input').click(function(e){
     const id = q.attr('data-id'), a = $(e.target), value = $("label[for="+a.attr('id')+"]").text();
 
     if(id == a.attr('name'))
-    if(value == 'Yes')
+    if(value == 'Yes'){
     $("div[data-main-question='"+id+"']").removeClass('disable-input');
+    $("div[data-main-question='"+id+"'] input").prop('required', true);
+    }
     else{
     $("div[data-main-question='"+id+"']").addClass('disable-input');
-    $("div[data-main-question='"+id+"'] input").val('')}
+    $("div[data-main-question='"+id+"'] input").val('').removeAttr('required');
+    }
     
 });
